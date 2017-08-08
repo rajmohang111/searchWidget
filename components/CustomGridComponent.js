d1f206d6c66f8d503b18164953c7aa42a9928194
Rajmohan G <rajmohan.g@ADRAPIDVALUE.COM>
2017-08-08 09:24:54 +0530

    add flexbox

--- components/CustomGridComponent.js
+++ components/CustomGridComponent.js
@@ -4,7 +4,6 @@ app.component("customGrid", {
     templateUrl: function ($element, $attrs) {
         return '/templates/customGridTpl.html'
     },
-   // template: '<div class="search-wrap"><input type="text" id="search-field" class="search-box" ng-model="$ctrl.searchText" placeholder="SEARCH (Client Name / Policy Number)"/></div> <grid-table name="$ctrl.searchText"></grid-table>',
     controller: function() {
         let ctrl = this;
         this.searchText = '';
